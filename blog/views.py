e70670a9138ce87d990f68325be80bec4631a926
JuliaMbamarah <mbamarahjulia@gmail.com>
2022-06-18 23:28:45 +0100

    Done

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 # Create your views here.
+from django.views.generic.edit import CreateView
+from .models import Post
+
+class PostListView(CreateView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url: reverse_lazy("blog:all")
+
+class PostDetailView(CreateView):
+    model = Post
+
+class PostUpdateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url: reverse_lazy("blog:all")
+
+class PostDeleteView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url: reverse_lazy("blog:all")
